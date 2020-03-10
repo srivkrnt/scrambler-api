9052dec5582c65c10fc336fddae1a8d9507cdc0d
srivkrnt <sri.vkrnt@gmail.com>
2020-03-10 18:50:40 +0530

    include difficulty

--- api.py
+++ api.py
@@ -3,20 +3,38 @@ import random
 
 app = Flask(__name__)
 
-noOfWords = 11800
 fileName = 'words.txt'
+easyWordFile = 'easy_words.txt'
 
-@app.route('/')
+
+@app.route('/all')
 def index():
     file = open(fileName)
     words = file.readlines()
-
+    noOfWords = len(words)
     wordIndex = (random.randint(2, noOfWords)//2) * 2
     response = {}
     response['index'] = wordIndex
     response['actual_word'] = words[wordIndex+1]
     response['scrambled_word'] = words[wordIndex]
     response['split_scrambled_word'] = list(''.join(words[wordIndex]).replace(' ','').strip())
+    response['noOfWords'] = noOfWords
+    return response
+
+@app.route('/')
+def easy_words():
+    file = open(easyWordFile)
+    words = file.readlines()
+    noOfWords = len(words)
+    wordIndex = (random.randint(2, noOfWords//2) * 2)
+    word = words[wordIndex].strip()
+    response = {}
+    response['index'] = wordIndex
+    response['actual_word'] = word
+    word = list(word)
+    random.shuffle(word)
+    response['split_scrambled_word'] = word
+    response['scrambled_word'] = ' '.join(word)
     return response
 
 @app.route('/scramble/<word>')
